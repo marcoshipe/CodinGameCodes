61fd8afc4307c2c1cb3a6de2c83504e072eacbd9
Marcos Hipe <marcoshipe@gmail.com>
2017-07-15 01:44:19 -0300

    Add unit tests Fix little warning

--- Python/Classic Puzzle - Easy/Horse-racing Duals/Solution.py
+++ Python/Classic Puzzle - Easy/Horse-racing Duals/Solution.py
@@ -1,13 +1,46 @@
-n = int(input())  # number of horses
-horses_strengths = []  # [horse_strength_1, horse_strength_2, ...]
-for i in range(n):
-    horses_strengths.append(int(input()))
+import argparse
+import unittest
+from io import StringIO
+from unittest.mock import patch
 
-horses_strengths.sort()
-min_dist = abs(horses_strengths[0] - horses_strengths[1])
-for i in range(1, len(horses_strengths)):
-    act_dist = abs(horses_strengths[i] - horses_strengths[i - 1])
-    if act_dist < min_dist:
-        min_dist = act_dist
 
-print(str(min_dist))
+def main():
+    n = int(input())  # number of horses
+    horses_strengths = []  # [horse_strength_1, horse_strength_2, ...]
+    for i in range(n):
+        horses_strengths.append(int(input()))
+
+    horses_strengths.sort()
+    min_dist = abs(horses_strengths[0] - horses_strengths[1])
+    for i in range(1, len(horses_strengths)):
+        act_dist = abs(horses_strengths[i] - horses_strengths[i - 1])
+        if act_dist < min_dist:
+            min_dist = act_dist
+
+    print(str(min_dist))
+
+
+class MyTestCase(unittest.TestCase):
+    def run_test_file(self, input_file_path, expected_output_file_path):
+        with open(input_file_path, 'r') as input_file, open(expected_output_file_path, 'r') as expected_output_file:
+            with patch('sys.stdin', input_file), patch('sys.stdout', new=StringIO()) as output:
+                main()
+                self.assertEqual(output.getvalue().strip(), expected_output_file.read())
+
+    def test_simple_test(self):
+        self.run_test_file('test_simple_test_input.txt', 'test_simple_test_expected_output.txt')
+
+    def test_horses_in_any_order(self):
+        self.run_test_file('test_horses_in_any_order_input.txt', 'test_horses_in_any_order_expected_output.txt')
+
+    def test_many_horses(self):
+        self.run_test_file('test_many_horses_input.txt', 'test_many_horses_expected_output.txt')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Resolution of \"Horse-racing Duals\" problem from codingame.com')
+    parser.add_argument('--unittest', help='run unit tests', action='store_true')
+    args = parser.parse_args()
+    if args.unittest:
+        unittest.main(argv=[argparse.ArgumentParser().prog])
+    else:
+        main()
--- Python/Classic Puzzle - Medium/Conway Sequence/Solution.py
+++ Python/Classic Puzzle - Medium/Conway Sequence/Solution.py
@@ -1,3 +1,18 @@
+import argparse
+import unittest
+from io import StringIO
+from unittest.mock import patch
+
+
+def main():
+    r = int(input())  # The original number R of the sequence.
+    l = int(input())  # The line L to display. The index of the first line is 1.
+    conway_sequence_list = [r]
+    for _ in range(l-1):
+        conway_sequence_list = conway_sequence_get_next_line(conway_sequence_list)
+    print(" ".join(str(x) for x in conway_sequence_list))
+
+
 def conway_sequence_get_next_line(actual_line):
     actual_ch = actual_line[0]
     amount = 0
@@ -14,10 +29,37 @@ def conway_sequence_get_next_line(actual_line):
     next_line.append(actual_ch)
     return next_line
 
-r = int(input())  # The original number R of the sequence.
-l = int(input())  # The line L to display. The index of the first line is 1.
 
-list = [r]
-for _ in range(l-1):
-    list = conway_sequence_get_next_line(list)
-print(" ".join(str(x) for x in list))
+class MyTestCase(unittest.TestCase):
+    def run_test_file(self, input_file_path, expected_output_file_path):
+        with open(input_file_path, 'r') as input_file, open(expected_output_file_path, 'r') as expected_output_file:
+            with patch('sys.stdin', input_file), patch('sys.stdout', new=StringIO()) as output:
+                main()
+                self.assertEqual(output.getvalue().strip(), expected_output_file.read())
+
+    def test_r_1_l_11(self):
+        self.run_test_file('test_r_1_l_11_input.txt', 'test_r_1_l_11_expected_output.txt')
+
+    def test_r_2_l_1(self):
+        self.run_test_file('test_r_2_l_1_input.txt', 'test_r_2_l_1_expected_output.txt')
+
+    def test_r_5_l_10(self):
+        self.run_test_file('test_r_5_l_10_input.txt', 'test_r_5_l_10_expected_output.txt')
+
+    def test_r_25_l_10(self):
+        self.run_test_file('test_r_25_l_10_input.txt', 'test_r_25_l_10_expected_output.txt')
+
+    def test_r_1_l_25(self):
+        self.run_test_file('test_r_1_l_25_input.txt', 'test_r_1_l_25_expected_output.txt')
+
+    def test_r_33_l_25(self):
+        self.run_test_file('test_r_33_l_25_input.txt', 'test_r_33_l_25_expected_output.txt')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Resolution of \"Conway Sequence\" problem from codingame.com')
+    parser.add_argument('--unittest', help='run unit tests', action='store_true')
+    args = parser.parse_args()
+    if args.unittest:
+        unittest.main(argv=[argparse.ArgumentParser().prog])
+    else:
+        main()
